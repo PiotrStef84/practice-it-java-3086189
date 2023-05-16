5cd2a2af567c3c27a082fb78891014e4e4130a7f
PiotrStef84 <77744484+PiotrStef84@users.noreply.github.com>
2023-05-16 18:43:19 +0000

    Tue_1

--- _01_02b/Employee.java
+++ _01_02b/Employee.java
@@ -5,7 +5,7 @@ public class Employee {
   public static void main(String[] args) {
 
     // Create a variable called age of type int and assign it the value 29.
-    int age = 20;
+    int age = 29;
 
     // Print the age variable to the console.
     System.out.println(age);
@@ -33,15 +33,20 @@ public class Employee {
 
     // Create a variable called totalPayment of type int and assign it to the value
     // of baseSalary added to overtimePayment.
+    int totalPayment = baseSalary + overtimePayment; 
 
     // Print the totalPayment variable to the console.
+    System.out.println(totalPayment);
 
     // Create three variables all of type double on a single line.
     // They should be called firstBonus, secondBonus and thirdBonus and they should
     // be assigned the values 10.00, 22.00 and 35.00.
+    double firstBonus = 10; double secondBonus = 22.00; double thirdBonus = 35.00;
 
     // Print out the sum of the variables called firstBonus, secondBonus and
     // thirdBonus.
+
+    System.out.println(firstBonus + secondBonus + thirdBonus);
 
   }
 
